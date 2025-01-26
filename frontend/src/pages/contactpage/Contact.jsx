ecd13f8f1cb6768a4ac2c9feaa6185b2548354de
Liza Malik <129332525+malik-liza0702@users.noreply.github.com>
2025-01-27 00:37:33 +0530

    Update Contact.jsx

--- frontend/src/pages/contactpage/Contact.jsx
+++ frontend/src/pages/contactpage/Contact.jsx
@@ -29,7 +29,7 @@ function Contact() {
       setContactMessage(myMessage);
     }
     try{
-      const response=await axios.post("http://localhost:4000/api/contact/create",contactMessage);
+      const response=await axios.post("https://gomedia-com.onrender.com/api/contact/create",contactMessage);
       console.log("contact created successfully..")
       toast.success("Message sent successfully")
 
